3db9c67bb622fdf880b1e1e9b5feb8ddf547815d
fondlykip <liam.j.campbell@hotmail.co.uk>
2025-03-02 13:26:07 +0100

    Dry Run Matching, updating playlists, and deleting matched tracks from Library Playlist

--- main.py
+++ main.py
@@ -3,27 +3,27 @@ Main entry point into the application
 """
 from src.helpers import itunes_utils_v2
 from pathlib import Path
-import copy
-from pywintypes import com_error
-import win32api
 
 def main():
-    DRY_RUN = False
+    DRY_RUN = True
     print("init Itunes Application")
     itunes = itunes_utils_v2.ITunesLibrary()
-    itunes_app = itunes.itunes
 
-    pt_oid = (69, 35908, 36052, 7130) # src_id, pl_id, t_id, t_db_id
- 
-    pt = itunes_app.GetITObjectByID(*pt_oid)
-    lpt = itunes.get_lib_track(pt)
-    # print("Get Itunes DFs")
-    # itunes.get_library_dfs()
-    # print("Run Matching")
-    # num_matches = itunes.run_matching()
-    # print(f"{num_matches} matches found")
-    # itunes.export_csvs(Path('./src/data/'))
-    # itunes.replace_matches(dry_run=DRY_RUN)
+    print("Get Itunes DFs")
+    itunes.get_library_dfs()
+
+    print("Run Matching")
+    num_matches = itunes.run_matching()
+    print(f"{num_matches} matches found")
+
+    print("export dfs for analysis")
+    itunes.export_csvs(Path('./src/data/'))
+
+    print(f"replaced matched fils in user playlists | dry_run {DRY_RUN}")
+    itunes.replace_matches(dry_run=DRY_RUN)
+
+    print(f"delete matched tracks from library_tracks | dry_run {DRY_RUN}")
+    itunes.delete_matched_library_tracks(dry_run=DRY_RUN)
     #itunes.test_playlist_operations()
 
 if __name__ == "__main__":
